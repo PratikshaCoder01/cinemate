e92f3972a7be3279e8a5e3175227bb6fb78a0c08
PratikshaCoder01 <pratikshapatil8987@gmail.com>
2024-10-23 21:30:03 +0530

    UseParams updated

--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -1,5 +1,5 @@
-import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import { useTitle } from "../hooks/useTitle";
 import Backup from "../assets/images/backup.png";
 
